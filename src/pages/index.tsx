050f83f85a3439d858f3dd7e0c85f984b942fca7
unknown <jhonnata1337@gmail.com>
2022-04-26 18:40:48 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +27,36 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+ return(
+   <>
+    <main className={commonStyles.container}>
+        <Header />
+
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum Titulo</strong>
+              <p>Pesandoem sincronização em vez de ciclos de vida </p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 mar 2022
+                </li>
+                <li>
+                  <FiUser />
+                  YoKim
+                </li>
+              </ul>
+            </a>
+          </Link>
+        <button type="button">Carregar mais posts</button>
+        </div>
+    </main>
+   
+   </>
+ )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
