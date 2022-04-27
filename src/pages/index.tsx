5606305b453be34ee36bf625bd2fe20430b2c942
unknown <jhonnata1337@gmail.com>
2022-04-27 09:47:04 -0300

    pequena alteraçoes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 import Header from '../components/Header';
+import Prismic from "@prismicio/client";
+import { format } from 'date-fns'
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -27,7 +30,15 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination  }:HomeProps) {
+  const formattedPost =postsPagination.results.map(post => {
+    return {
+      ...post,
+      
+    }
+  })
+  const [posts, setPosts] = useState();
+
  return(
    <>
     <main className={commonStyles.container}>
@@ -58,9 +69,35 @@ export default function Home() {
  )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+ const prismic = getPrismicClient();
+ const postsResponse = await prismic.query<any>(
+   [Prismic.Predicates.at('document.type','posts')],
+   {
+     pageSize: 1,
+   }
+  );
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        substitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  }
+
+   return {
+     props:{
+      postsPagination,
+     }
+   }
+ };
